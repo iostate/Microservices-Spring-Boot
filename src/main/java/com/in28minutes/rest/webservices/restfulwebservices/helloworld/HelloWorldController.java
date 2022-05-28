06106ddcd0a6e75787cfd2829e3050e99f9cd682
iostate <qmtruong92@gmail.com>
2022-05-28 01:47:03 -0700

    I miss Java

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
-package com.in28minutes.rest.webservices.restfulwebservices;
+package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 // Controller
@@ -11,17 +12,20 @@ public class HelloWorldController {
     // Get
     // URI - /hello-world
     // method - return "Hello, World"
-     @GetMapping( path = "/hello_world")
+     @GetMapping( path = "/hello-world")
      public String helloWorld() {
          return "Hello World";
      }
 
     // FURTHER SIMPLIFY RequestMapping to @GetMapping, example below
     // Notice there's no need for method = RequestMethod.GET
-    @GetMapping(path = "/hello_world-bean")
+    @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
     }
 
-    // Similarly, there's PostMapping, etc
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
+         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
 }
---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,46 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+    private static List<User> users = new ArrayList<>();
+
+    private static int usersCount = 3;
+
+    static {
+        users.add(new User(1, "Adam", new Date()));
+        users.add(new User(2, "Eve", new Date()));
+        users.add(new User(3, "Eric", new Date()));
+
+    }
+
+     public List<User> findAll() {
+        return users;
+     }
+
+    /**
+     * Returns true if the User was added. False if not.
+     * @param user User to be added to users.
+     * @return boolean True if the User was added
+     */
+     public boolean save(User user) {
+        // if the user has no id
+        if (user.getId()==null) {
+            user.setId(++usersCount);
+        }
+        return users.add(user);
+     }
+     public User findOne(int id) {
+         for (User user:users) {
+             if(user.getId()==id) {
+                 return user;
+             }
+         }
+         return null;
+     }
+
+}
