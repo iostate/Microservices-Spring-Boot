6346455e85ad26caef2a8595f7a4040b1cc20223
iostate <qmtruong92@gmail.com>
2022-05-30 12:56:35 -0700

    Add Exception Handling

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,16 +1,7 @@
-package com.in28minutes.rest.webservices.restfulwebservices.exception;
-
-import com.in28minutes.rest.webservices.restfulwebservices.user.UserNotFoundException;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.util.Date;
-
 /**
+ * Controls all Exception Handling for all Exceptions thrown.
+ * Create customized Exception Messages for different resources.
+ *
  * A controller advice allows you to use exactly the same exception handling techniques but apply them across the whole application, not just to an individual controller. You can think of them as an annotation driven interceptor.
  *
  * Any class annotated with @ControllerAdvice becomes a controller-advice and three types of method are supported:
@@ -21,14 +12,40 @@ import java.util.Date;
  * Binder initialization methods (used for configuring form-handling) annotated with
  * @InitBinder.
  */
+package com.in28minutes.rest.webservices.restfulwebservices.exception;
+
+import com.in28minutes.rest.webservices.restfulwebservices.user.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
 
 @ControllerAdvice
 @RestController
-public class StandardizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public final ResponseEntity<Object> handleAllExceptions (UserNotFoundException ex, WebRequest request) {
+    public final ResponseEntity<Object> handleAllExceptions
+            (Exception ex, WebRequest request) throws Exception {
         // return Date, Message, Details inside of a ExceptionResponse
-        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), new ex.getMessage(), new ex.getDescription(false));
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<Object> handleAllExceptions
+            (UserNotFoundException ex, WebRequest request) throws Exception {
+        // return Date, Message, Details inside of a ExceptionResponse
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/ExceptionResponse.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/ExceptionResponse.java
@@ -1,2 +1,59 @@
-package com.in28minutes.rest.webservices.restfulwebservices.exception;public class ExceptionResponse {
+package com.in28minutes.rest.webservices.restfulwebservices.exception;
+
+import java.util.Date;
+
+public class ExceptionResponse {
+    // The most basic properties of an exception are:
+    // date
+    private Date timestamp;
+    // message
+    private String message;
+
+    // details
+    private String details;
+
+    /**
+     * A standardized Exception Response to be used across an entire enterprise application.
+     * Users get a uniform Response which they can grow accustomed to.
+     * @param timestamp timestamp of the ExceptionResponse
+     * @param message message of the ExceptionResponse
+     * @param details details of the ExceptionResponse
+     */
+    public ExceptionResponse(Date timestamp, String message, String details) {
+
+        /**
+         * @TODO Ideally, map an FN() to a ROUTE that renders a template for XXX HTTP Code.
+         * Developers want to have a uniform Response for certain HTTP Response codes.
+         * Developers also must make sure to hide the implementation details of their
+         */
+        super();
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+
+    /**
+     * Get the timestamp of the ExceptionResponse
+     * @return java.util.Date - A specific instant in time, with millisecond precision
+     */
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    /**
+     * Get the message of the ExceptionResponse
+     * @return String - The message of the ExceptionResponse
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * Get the details of the ExceptionResponse
+     * @return String Details of the ExceptionResponse
+     */
+    public String getDetails() {
+        return details;
+    }
+
 }
